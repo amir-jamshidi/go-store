daf891d7339a29d08ecfdfa4de6e7eaa5ec79e68
Amir Jamshidi <aabbccddee9898@gmail.com>
2026-01-16 00:37:28 +0330

    implement repositories

---
+++ repository/mysql/user.go
@@ -0,0 +1,10 @@
+package mysql
+
+import "room-reserve/entity"
+
+func (db DB) IsPhoneNumberUnique(phoneNumber string) (bool, error) {
+
+}
+func (db DB) RegisterUser(u entity.User) (entity.User, error) {
+
+}
