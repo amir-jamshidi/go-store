23103330d217f882fcad364d233385c3289cda70
Amir Jamshidi <aabbccddee9898@gmail.com>
2026-02-05 01:37:23 +0330

    create base game

--- repository/mysql/user.go
+++ repository/mysql/user.go
@@ -3,8 +3,8 @@ package mysql
 import "room-reserve/entity"
 
 func (db DB) IsPhoneNumberUnique(phoneNumber string) (bool, error) {
-
+	return false, nil
 }
 func (db DB) RegisterUser(u entity.User) (entity.User, error) {
-
+	return entity.User{}, nil
 }
